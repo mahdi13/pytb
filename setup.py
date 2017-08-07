88d869a55914a5040465dcfb315bffd5718b4239
perfect <m.aali.pro@gmail.com>
2017-08-07 18:55:32 +0430

    init

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
-import re
-from os.path import join, dirname
 from setuptools import setup, find_packages
 
-# reading package version (same way the sqlalchemy does)
-with open(join(dirname(__file__), 'pytb', '__init__.py')) as v_file:
-    package_version = 'v0.1.1'
+package_version = 'v0.1.1'
 
 dependencies = [
     'python-telegram-bot',
