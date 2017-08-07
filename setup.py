45681a5f9425d3fe6ec247e3934b0845643f4dfc
perfect <m.aali.pro@gmail.com>
2017-08-07 18:54:05 +0430

    init

---
+++ setup.py
@@ -0,0 +1,28 @@
+import re
+from os.path import join, dirname
+from setuptools import setup, find_packages
+
+# reading package version (same way the sqlalchemy does)
+with open(join(dirname(__file__), 'pytb', '__init__.py')) as v_file:
+    package_version = re.compile(r".*__version__ = '(.*?)'", re.S).match(v_file.read()).group(1)
+
+dependencies = [
+    'python-telegram-bot',
+    'ujson',
+    'requests',
+    'webtest',
+    'nose'
+]
+
+setup(
+    name="pytb",
+    version=package_version,
+    author="Perfect",
+    author_email="mahdi_1373@yahoo.com",
+    install_requires=dependencies,
+    packages=find_packages(),
+    test_suite="pytb.tests",
+    message_extractors={'pytb': [
+        ('**.py', 'python', None),
+    ]},
+)
